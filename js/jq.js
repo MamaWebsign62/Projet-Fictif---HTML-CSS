3170e31c8ef8b042f6c4683c1b4fbda3f0b6946f
MamaWebsign62 <mregnault.netgyver@gmail.com>
2017-12-07 15:58:49 +0100

    modif js

--- js/jq.js
+++ js/jq.js
@@ -3,45 +3,45 @@
 /*JQUERY*/
 
 $(function(){
-            
-            
-            /*SCROLL HEADER*/
-            
-            $('#scroll-header').css({'display':'none'});
 
-            $(window).scroll(
-                function(){
-                    var maPosScroll = $(document).scrollTop();
 
-                    if(maPosScroll >=100){
-                        $('#scroll-header').slideDown(300);
-                    }
+            // /*SCROLL HEADER*/
+            //
+            // $('#scroll-header').css({'display':'none'});
+            //
+            // $(window).scroll(
+            //     function(){
+            //         var maPosScroll = $(document).scrollTop();
+            //
+            //         if(maPosScroll >=100){
+            //             $('#scroll-header').slideDown(300);
+            //         }
+            //
+            //         else{
+            //             $('#scroll-header').slideUp(300);
+            //         }
+            //
+            //     }
+            // );
 
-                    else{
-                        $('#scroll-header').slideUp(300);
-                    }
-            
-                }
-            ); 
-    
-    
-    
-    
-            
-        /*BACK TOP*/   
 
-           
+
+
+
+        /*BACK TOP*/
+
+
         $('#back-top').hide();
-        $(function(){ 
-            
+        $(function(){
+
             $(window).scroll(
-                
+
                 function() {
-                    
+
                     if ($(this).scrollTop() > 100) {
                     $('#back-top').fadeIn();
                 }
-                
+
                 else {
                     $('#back-top').fadeOut();
                 }
@@ -54,21 +54,17 @@ $(function(){
                         return false;
                     }
                 );
-    
-    
-        });   
-    
-	
+        });
+
+
 	/*SOUS-MENU*/
-	
-    $('nav ul li a').mouseenter(function(){
-		$(this).next().fadeIn(400);
-	});
-   
-	$('.sous-menu').mouseleave(function(){
-		$(this).fadeOut(400);
-	})
-	
+
+  //   $('nav ul li a').mouseenter(function(){
+	// 	$(this).next().fadeIn(400);
+	// });
+  //
+	// $('.sous-menu').mouseleave(function(){
+	// 	$(this).fadeOut(400);
+	// })
+
 });
-
-
